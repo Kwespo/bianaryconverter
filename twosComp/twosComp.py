df97ff98b9614c43b6eb72856ce5ed30530eff92
Kwespo <73428848+Kwespo@users.noreply.github.com>
2023-10-16 00:29:56 +0100

    worked heavily on the twos comp Still need to finish the adding one.

--- twosComp/twosComp.py
+++ twosComp/twosComp.py
@@ -1,17 +1,58 @@
+
 bInp = input("Input your binary list (Separated by spaces): ") # input the binary
 
+revInp = ["1", ]
 
-bList = bInp.split()
 
-for num in bList:
+#validation:
+"""
+256 128 64 32 16 8 4 2 1
+"""
+if len(bInp.strip()) > 8: 
+  print("Number is too large. This is using 8 bit bianary")
 
-  if num == "0":
-    num += "1"
+
+
+# Flipping the bits
+def flip():
+  """
+  Flipping the bits
+  """
+  global bInp, revInp
+
+  for num in bInp:
+
+    if num == "1":
+        revInp += "0"
+
+    elif num == "0":
+      revInp += "1"
+
+    elif num == " " or num == ",":
+      pass
+
+    else:
+      print("Invalid number")
+    
+  addbit()
+
+#adding the bits
+
+def addbit():
+  """
+  Adding the 1 bit to the end of the list
+  """
+  if revInp[-1] == "0":
+    revInp[-1] = "1"
   
-  elif num == "1":
-    num += "0"
-
   else:
-    print("Invalid number")
+    n = -1
 
-print(bList)
+    """
+    Loop though the list in reverse till you find the first 0 and the change all 1s from there to a 0.
+    """  
+
+
+
+if __name__ == '__main__':
+  flip()
