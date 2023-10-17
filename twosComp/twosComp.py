cac349f0ab2d178fed8d193b2878d32aed63debe
Kwespo <pikej1088@gmail.com>
2023-10-17 21:58:31 +0100

    yes

--- twosComp/twosComp.py
+++ twosComp/twosComp.py
@@ -1,7 +1,7 @@
 
 bInp = input("Input your binary list (Separated by spaces): ") # input the binary
 
-revInp = ["1", ]
+bList = ["1", ]
 
 
 #validation:
@@ -18,15 +18,15 @@ def flip():
   """
   Flipping the bits
   """
-  global bInp, revInp
+  global bInp, bList
 
   for num in bInp:
 
     if num == "1":
-        revInp += "0"
+        bList += "0"
 
     elif num == "0":
-      revInp += "1"
+      bList += "1"
 
     elif num == " " or num == ",":
       pass
@@ -34,25 +34,31 @@ def flip():
     else:
       print("Invalid number")
     
-  addbit()
 
 #adding the bits
 
 def addbit():
+  global bList
   """
   Adding the 1 bit to the end of the list
   """
-  if revInp[-1] == "0":
-    revInp[-1] = "1"
+  if bList[-1] == "0":
+    bList[-1] = "1"
   
   else:
-    n = -1
-
+    n = 0
     """
     Loop though the list in reverse till you find the first 0 and the change all 1s from there to a 0.
     """  
 
-
+    bList.reverse()
+    while True:
+      if bList[n] == "0": 
+        bList[:0] = "0"
+        break
+      else: 
+        n += 1
 
 if __name__ == '__main__':
   flip()
+  addbit()
